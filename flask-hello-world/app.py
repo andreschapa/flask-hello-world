fd0f7d947d24cbcef6e673be2c82d30b4c950f71
andreschapa <andres@scanmatics.com>
2020-12-05 16:09:06 -0600

    Shit up to page 48

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -4,17 +4,41 @@ from flask import Flask
 #create the application object
 app=Flask(__name__)
 
+#error handling
+app.config["DEBUG"]= True
 #use the decorator pattern to link the view function to a URL
 @app.route("/")
 @app.route("/hello")
 
 #define the view function which returns a string
 def hello_world():
-    return"FUCK YOU"
+    return"YOOOOOOOOOOOOOOOOOOOOOO"
 
 #start the development server using the run() method
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1) 
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower()=="michael":
+        return f"Hello, {name}", 200
+    else:
+        return "Not Found",404
+
 
 if __name__== "__main__":
     app.run()
 
-##hello
